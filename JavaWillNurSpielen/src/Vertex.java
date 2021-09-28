8c7b6e67b8ccf043ee1e531ef18be9129764c42d
AdrianNenaschew <AdrianNenaschew@0-208-12>
2021-09-28 15:25:34 +0200

    Dienstag Vertex

--- JavaWillNurSpielen/src/Vertex.java
+++ JavaWillNurSpielen/src/Vertex.java
@@ -1,4 +1,3 @@
-
 public class Vertex {
 	public double x;
 	public double y;
@@ -6,11 +5,48 @@ public class Vertex {
 	public Vertex(double x, double y) {
 		this.x = x;
 		this.y = y;
-		
 	}
-	 public String toString() {
-		 
-		 
-		 return "x ="+x+", y="+y; 
-	 }
+
+	public String toString() {
+		return "x = " + x + ", y= " + y;
+	}
+
+	public double length() {
+		return Math.sqrt(x * x + y * y);
+	}
+
+	public Vertex skalarMult(double s) {
+		return new Vertex(x * s, y * s);
+	}
+	
+	public void skalarMultMod (double s) {
+		x=s*x;
+		y=s*y;
+	}
+
+	public Vertex add (Vertex v2) {
+		return new Vertex (x+v2.x,y+v2.y);
+	}
+	
+	public void addMod(Vertex v2) {
+		x=x+v2.x;
+		y=y+v2.y;
+	}
+	
+	public void setX(double x) {
+		this.x=x;
+	}
+	
+	public void setY(double y) {
+		this.y=y;
+	}
+	
+	public double getX(double x) {
+		return x;
+	}
+	
+	public double getY(double y) {
+		return y;
+	}
 }
+
