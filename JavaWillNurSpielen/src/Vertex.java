7d0ca8a3fd9eaba14a65cd00afaea502af294c81
AdrianNenaschew <AdrianNenaschew@0-209-R13>
2021-10-01 09:08:55 +0200

    Abschluss Vertex

--- JavaWillNurSpielen/src/Vertex.java
+++ JavaWillNurSpielen/src/Vertex.java
@@ -8,7 +8,7 @@ public class Vertex {
 	}
 
 	public String toString() {
-		return "x = " + x + ", y= " + y;
+		return "(" + x + ",  " + y + ")";
 	}
 
 	public double length() {
@@ -18,35 +18,61 @@ public class Vertex {
 	public Vertex skalarMult(double s) {
 		return new Vertex(x * s, y * s);
 	}
-	
-	public void skalarMultMod (double s) {
-		x=s*x;
-		y=s*y;
+
+	public void skalarMultMod(double s) {
+		x = s * x;
+		y = s * y;
 	}
 
-	public Vertex add (Vertex v2) {
-		return new Vertex (x+v2.x,y+v2.y);
+	public Vertex add(Vertex that) {
+		return new Vertex(x + that.x, y + that.y);
 	}
-	
-	public void addMod(Vertex v2) {
-		x=x+v2.x;
-		y=y+v2.y;
+
+	public void addMod(Vertex that) {
+		x = x + that.x;
+		y = y + that.y;
 	}
-	
+
 	public void setX(double x) {
-		this.x=x;
+		this.x = x;
 	}
-	
+
 	public void setY(double y) {
-		this.y=y;
+		this.y = y;
 	}
-	
+
 	public double getX(double x) {
 		return x;
 	}
-	
+
 	public double getY(double y) {
 		return y;
 	}
-}
 
+	public boolean equals(Object thatObject) {
+		if (thatObject instanceof Vertex) {
+			Vertex that = (Vertex) thatObject;
+			return this.x == that.x && this.y == that.y;
+		} else {
+			return false;
+		}
+
+	}
+
+	public Vertex sub(Vertex that) {
+		return new Vertex(x - that.x, y - that.y);
+	}
+
+	public double distance(Vertex that) {
+		double a = that.x - this.x;
+		double b = that.y - this.y;
+		return Math.sqrt(a * a + b * b);
+	}
+
+	public void normalize() {
+		double factor = this.length();
+		this.x = x / factor;
+		this.y = y / factor;
+	}
+
+}
