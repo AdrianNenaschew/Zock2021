90914b4b635e60f9728e5178505c90d2a435122b
AdrianNenaschew <AdrianNenaschew@0-208-12>
2021-11-23 15:25:36 +0100

    New Panels

---
+++ JavaWillNurSpielen/src/SizedPanel.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+import util.ShowInFrame;
+
+import java.awt.Dimension;
+
+public class SizedPanel extends JPanel {
+	int preferredWidth = 400;
+	int preferredHeight = 300;
+
+
+	public SizedPanel(int w, int h){
+		int preferredWidth = w;
+		int prefferedHeight = h;
+	}
+
+	public SizedPanel() {}
+	
+	@Override 
+	public Dimension getPreferredSize() {
+		return new Dimension (preferredWidth,preferredHeight);
+	}
+	
+	public static void main (String[] args) {
+		ShowInFrame.show(new SizedPanel());
+	}
+}
