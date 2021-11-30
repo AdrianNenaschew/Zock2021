087e8a509540fc48295df38acdf5f85621d611b6
AdrianNenaschew <AdrianNenaschew@0-208-12>
2021-11-30 15:28:32 +0100

    ok

--- JavaWillNurSpielen/src/MovableImage.java
+++ JavaWillNurSpielen/src/MovableImage.java
@@ -1,7 +1,36 @@
+import javax.swing.ImageIcon;
+public class MovableImage extends GeometricImage implements GameObject{
+	
+Vertex deltaPos = new Vertex (0,0);
+public Vertex getDeltaPos () {
+	return deltaPos;
+}
+public void setDeltaPos(Vertex v) {
+	deltaPos=v;
+}
+public MovableImage(String name, double x, double y) {
+	super(name, x, y);
+}
+public MovableImage (String name, double x, double y, double dX, double dY) {
+	super(name, x,y);
+	deltaPos=new Vertex(dX,dY);
+}
+public MovableImage (ImageIcon icon, double x, double y, double dX, double dY) {
+	super(icon, x,y);
+	deltaPos=new Vertex(dX,dY);
+}
 
-public class MovableImage {
+public void move() {
+	pos.addMod(deltaPos);
+}
+public void turn() {
+	deltaPos.skalarMultMod(-1);
+}
+	
 
-	public static void main(String[] args) {
+
+
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
